e881f7acb8f80cf84e4bf0a2206b2923bea53cbc
VincenzoKhoa <khoa.vm.512@gmail.com>
2022-03-25 15:05:51 +0700

    mm

---
+++ hocvejs/myScript.js
@@ -0,0 +1,46 @@
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed";
+// }
+//
+//
+// // prompt("How are you?");
+// // alert("hello");
+// // document.write("ban da san sang nhan ket qua")
+// //
+// // confirm("ban bi ung thư");
+//
+//
+// let name = prompt("nhap ten vao");
+// // alert(name);
+// document.write(name);
+// // console.log(name);
+// document.getElementById("demo").innerHTML = name;
+
+// const pass = 4.5;
+// const math = 9;
+// const phys = 9.5;
+// const mec = 8;
+//
+// function do_something() {
+//     const selectValue = document.getElementById("mySelect").value;
+//     alert(selectValue)
+// }
+
+// const selectedElement = document.getElementById("mySelect");
+// selectedElement.addEventListener("change", changeFunction);
+// function changeFunction() {
+//     const selectValue = document.getElementById("mySelect").value;
+//     alert(selectValue);
+// }
+// const demo = document.getElementById("demo");
+// demo.addEventListener("mouseover",mouseover);
+// demo.addEventListener("mouseout",mouseout)
+// function mouseover() {
+//     document.getElementById("demo").style.color = "red";
+// }
+// function  mouseout () {
+//     document.getElementById("demo").style.color = "black";
+// }
+// function showMessage() {
+//     document.getElementById("messageDiv").innerHTML = 'Xin Chao m!';
+// }
